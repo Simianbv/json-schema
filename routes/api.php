295babbaf2e45109894f0b835691e86e09045ba1
Merijn Venema <merijn.venema@simian.nl>
2020-04-14 17:06:47 +0200

    Added the old route to the route file

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ Route::group(['middleware' => ['api', 'introspect']], function () {
     Route::get('/api/schema/layout/{scope}/{resource?}', '\Simianbv\JsonSchema\Http\SchemaController@getLayout');
     Route::get('/api/schema/filters/{scope}/{resource?}', '\Simianbv\JsonSchema\Http\FilterController@index');
 
+    Route::get('/api/filters/{model}',  '\Simianbv\JsonSchema\Http\FilterController@getFiltersByModel');
+    Route::get('/api/filters',          '\Simianbv\JsonSchema\Http\FilterController@getFiltersByModel');
+
+
 });
 
 
